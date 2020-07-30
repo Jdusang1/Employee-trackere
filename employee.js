de6c8c7a7ced4388c144f17ca55cb28ac817bec1
JohnDusang <Johndusang1@gmail.com>
2020-07-29 19:44:05 -0600

    pushing for some debugging assistance

--- employee.js
+++ employee.js
@@ -8,6 +8,7 @@ const connection = mysql.createConnection({
     database: "employee_db"
 });
 
+
 const start = () => {
     inquirer.prompt([
         {
@@ -37,7 +38,7 @@ const start = () => {
                 case "View departments":
                     viewDepartments();
                     break;
-                case "View Roles":
+                case "View roles":
                     viewRoles();
                     break;
                 case "View all employees":
@@ -111,13 +112,10 @@ function addEmployee() {
     ]).then((data) => {
         let firstName = data.employeeFirstName;
         let lastName = data.employeeLastName;
-        connection.query("INSERT INTO employee SET ? WHERE ?",
+        connection.query("INSERT INTO employee SET ?",
             {
-                firstName: firstName
-
-            },
-            {
-                lastName: lastName
+                first_name: firstName,
+                last_name: lastName
 
             },
             (err) => {
@@ -128,19 +126,90 @@ function addEmployee() {
 
         )
     })
+};
+
+const viewDepartments = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "Please enter a department to search for:"
+
+        }
+    ]).then((data) => {
+        const name = data.department
+        connection.query("SELECT * FROM department WHERE name = ?", [name], (err, data) => {
+
+            if (err) throw err;
+            console.table(data);
+            start();
+        })
+
+    })
+
+};
+
+const viewRoles = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "role",
+            message: "Pleas enter a role you would like to view:"
+        }
+    ]).then((data) => {
+        const title = data.role;
+        connection.query("SELECT * FROM employee WHERE title= ?", [title], (err, data) => {
+
+            if (err) throw err;
+            console.table(data);
+            start();
+        })
+    })
+};
+
+const viewAllEmployees = () => {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        start();
+    })
+
 }
 
+const updateEmployeeRoles = async () => {
+    const employee = await connection.query("SELECT * FROM employee");
+    const role = await connection.query("SELECT * FROM role");
 
+    const data = await inquirer.prompt([{
+        type: "list",
+        name: "chosenEmployee",
+        message: "Which employee would you like to update?",
+        choices: employee.map(employee => ({
+            name: employee.first_name + " " + employee.last_name,
+            value: employee.id
+        }))
+    },
+    {
+        type: "rawlist",
+        name: "newRole",
+        message: "What is the employees new role?",
+        choices: role.map(role => ({
+            name: role.title,
+            value: role.id
+        }))
+    }
+    ])
 
-
-
-
-
-
-
-
-
-
+    const res = await connection.query("UPDATE employee SET ? WHERE ?", [{
+        role_id: data.newRole
+    },
+    {
+        id: data.chosenEmployee
+    }
+    ])
+    console.log(`\n${res.affectedRows} has been updated.\n`);
+    start();
+};
 
 
 connection.connect((err) => {
